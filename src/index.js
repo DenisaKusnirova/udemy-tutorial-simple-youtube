1567f2b401b6f2e57a99df2e190ce65d42d6559f
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-04-05 10:55:06 +0200

    Build video list of video list items with map method

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
-import SearchBar from './components/search_bar';
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 
 const API_KEY = 'AIzaSyCl9Ai4rroV2j-U551gOdR4hr9EqL8zLe8'
 
-YTSearch({ key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);  
-})
-
 class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            videos: []
+        }
+
+        YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos })  
+            // this.setState({ videos: videos })
+        })
+    }
+    
     render() {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos} />
             </div>
         )
     }
